13010d1cb83c5ff51fd0991b764575278a0e812d
Alisson Chiquitto <chiquitto@gmail.com>
2018-03-26 10:23:59 -0400

    Seed tabela Categoria

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Blog\Categoria;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(CategoriaTableSeeder::class);
     }
+}
+
+class CategoriaTableSeeder extends Seeder {
+
+    public function run() {
+        $sql = 'insert into categorias
+        (categoria, descricao, situacao, created_at, updated_at)
+values (?,?,?,?,?)';
+
+        $agora = date('Y-m-d H:i:s');
+
+        DB::insert($sql, ['Jogos', 'Jogos', Categoria::SITUACAO_ATIVO, $agora, $agora]);
+        DB::insert($sql, ['Política', 'Política', Categoria::SITUACAO_ATIVO, $agora, $agora]);
+        DB::insert($sql, ['Esportes', 'Esportes', Categoria::SITUACAO_INATIVO, $agora, $agora]);
+    }
+
 }
