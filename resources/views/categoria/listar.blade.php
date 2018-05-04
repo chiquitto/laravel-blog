047fac8a4f05a0e2b15793b158cd83a58eb7fb64
Alisson Chiquitto <chiquitto@gmail.com>
2018-05-04 18:39:57 -0400

    i18n

--- resources/views/categoria/listar.blade.php
+++ resources/views/categoria/listar.blade.php
@@ -1,30 +1,52 @@
-<h1>Categorias</h1>
+@extends('layouts.app')
 
-<p>
-    [<a href="{{ route('categoria-novo-form') }}">Nova categoria</a>]
-</p>
+@section('content')
 
-<table>
-    <thead>
-    <tr>
-        <th>#</th>
-        <th>Categoria</th>
-        <th></th>
-    </tr>
-    </thead>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
 
-    <tbody>
+                <h1>Categorias</h1>
 
-    @foreach ($categorias as $categoria)
-    <tr>
-        <td>{{ $categoria->idCategoria }}</td>
-        <td>{{ $categoria->categoria }}</td>
-        <td>
-            [<a href="{{ route('categoria-editar-form', ['id' => $categoria->idCategoria]) }}">Editar</a>]
-            [<a href="{{ route('categoria-apagar', ['id' => $categoria->idCategoria]) }}">Apagar</a>]
-        </td>
-    </tr>
-    @endforeach
+                @if (count($errors) > 0)
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                @endif
 
-    </tbody>
-</table>
+                <p>
+                    [<a href="{{ route('categoria-novo-form') }}">Nova categoria</a>]
+                </p>
+
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Categoria</th>
+                        <th></th>
+                    </tr>
+                    </thead>
+
+                    <tbody>
+
+                    @foreach ($categorias as $categoria)
+                        <tr>
+                            <td>{{ $categoria->idCategoria }}</td>
+                            <td>{{ $categoria->categoria }}</td>
+                            <td>
+                                [<a href="{{ route('categoria-editar-form', ['id' => $categoria->idCategoria]) }}">Editar</a>]
+                                [<a data-confirm="Are you sure to delete this item?" href="{{ route('categoria-apagar', ['id' => $categoria->idCategoria]) }}">Apagar</a>]
+                            </td>
+                        </tr>
+                    @endforeach
+
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+    </div>
+
+@endsection
